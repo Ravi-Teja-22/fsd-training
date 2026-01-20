e8613354f81c2691bc6b07ab723f1c65007ecea9
Ravi Teja CH <ravitejachinnabathini7@gmail.com>
2026-01-20 11:28:50 +0530

    react started

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+<>
+<Navbar />
+<Skills/>
+<Home/>
+<About/>
+</>
+  );
+}
+export default App;
